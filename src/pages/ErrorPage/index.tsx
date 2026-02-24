b4d7922a637656f4c69f88d6bf0974573354ee7b
Liza Emelyanova <liza.yemelyanova@list.ru>
2026-02-24 23:31:58 +0300

    :sparkles: create error page

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,9 +1,14 @@
-import { PageContainer } from "@/components"
+import { Button, PageContainer } from "@/components"
+import { useNavigate } from "react-router-dom"
 
 const ErrorPage = () => {
+    const navigate = useNavigate()
+
     return (
         <PageContainer>
-            Error Page
+            <h1 style={{ marginBottom: '10px' }}>404</h1>
+            <p style={{ marginBottom: '40px' }}>Page not found</p>
+            <Button onClick={() => navigate('/')}>Back to main</Button>
         </PageContainer>
     )
 }
